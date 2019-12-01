c8e47daac7d0ca8ee266a8944500f5d1e906cb22
Joao Pedro Bretanha <jpbretanha@gmail.com>
2019-12-01 11:41:38 -0300

    fix(container): media queries

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -5,24 +5,20 @@ const Container = styled.div`
   margin-left: auto;
   margin-right: auto;
 
-  @media (min-width: 576px) {
-    max-width: 540px;
+  @media (min-width: 480px) {
+    max-width: 478px;
   }
 
   @media (min-width: 768px) {
-    max-width: 720px;
+    max-width: 766px;
   }
 
   @media (min-width: 992px) {
-    max-width: 960px;
+    max-width: 990px;
   }
 
   @media (min-width: 1200px) {
     max-width: 1140px;
-  }
-
-  @media (min-width: 1920px) {
-    max-width: 1650px;
   }
 `
 
